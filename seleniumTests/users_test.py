e0ce4e82258165b18268383809e470b4f5a1595d
Jessica Sadler <jessicagsadler@gmail.com>
2021-05-07 09:28:53 +0200

    modified user test

--- seleniumTests/users_test.py
+++ seleniumTests/users_test.py
@@ -23,27 +23,29 @@ def test_create_user(): # create new user
     submit_button = driver.find_element_by_id("user-name-submit")
     submit_button.click()
 
-# def select_user():
-
-def test_user_name_taken():
-    driver.get("http://localhost/#/user-create")
-
-    name_box = driver.find_element_by_xpath("/html/body/div/div[2]/form/input")
-    name_box.send_keys("test")
-    driver.implicitly_wait(5)
-
-    submit_button = driver.find_element_by_id("user-name-submit")
-    submit_button.click()
-    driver.implicitly_wait(5)
-
-    error = driver.find_element_by_class_name("submission-error")
-
-    if error.is_displayed():
-        print("error message displayed")
-    else:
-        print("error message not displayed")
-
     driver.quit()
 
-    # assert an error message comes up
-    assert error.is_displayed(), "No error message"
+# def select_user():
+
+# def test_user_name_taken():
+#     driver.get("http://localhost/#/user-create")
+
+#     name_box = driver.find_element_by_xpath("/html/body/div/div[2]/form/input")
+#     name_box.send_keys("test")
+#     driver.implicitly_wait(5)
+
+#     submit_button = driver.find_element_by_id("user-name-submit")
+#     submit_button.click()
+#     driver.implicitly_wait(5)
+
+#     error = driver.find_element_by_class_name("submission-error")
+
+#     if error.is_displayed():
+#         print("error message displayed")
+#     else:
+#         print("error message not displayed")
+
+#     driver.quit()
+
+#     # assert an error message comes up
+#     assert error.is_displayed(), "No error message"
