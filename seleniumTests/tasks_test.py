e5c882d493d39d6dbcdf5cad8865f800de206d0a
Jessica Sadler <jessicagsadler@gmail.com>
2021-05-18 12:23:45 +0200

    added print task names def

--- seleniumTests/tasks_test.py
+++ seleniumTests/tasks_test.py
@@ -2,6 +2,7 @@ import os
 from selenium import webdriver
 import time
 import datetime
+import collections
 
 # open browser
 chromedriver = "/usr/local/bin/chromedriver"
@@ -10,12 +11,12 @@ driver = webdriver.Chrome(chromedriver)
 driver.get("http://localhost/")
 
 # begin test
-def test_goto_taskspage():
+def test_goto_taskspage(): # go to task page
     tasks = driver.find_element_by_link_text("Tasks")
     tasks.click()
     driver.implicitly_wait(10)
 
-def test_create_task():
+def test_create_task(): # create new task with unique name
     tasks = driver.find_element_by_link_text("Tasks")
     tasks.click()
 
@@ -30,8 +31,6 @@ def test_create_task():
     submit_button = driver.find_element_by_id("task-title-submit")
     submit_button.click()
 
-# def test_duplicate_tasks(): # does app allow duplicate tasks to be created
-
 def test_count_tasks(): # counts how many tasks have been created
     items = driver.find_elements_by_tag_name("li")
     count = 0
@@ -43,17 +42,27 @@ def test_count_tasks(): # counts how many tasks have been created
 
     print("There are " + str(count) + " tasks.")
 
-    # https://stackoverflow.com/questions/9835762/how-do-i-find-the-duplicates-in-a-list-and-create-another-list-with-them
-    # finds dupes in list
-    # seen = {}
-    # dupes = []
-    #     for x in a:
-    #         if x not in seen:
-    #             seen[x] = 1
-    #     else:
-    #         if seen[x] == 1:
-    #             dupes.append(x)
-    #             seen[x] += 1
+def test_list(): # works, but prints a lot of web element stuff, comment out for now.
+    a = driver.find_elements_by_tag_name("li")
+    task_names = [x.text for x in a]
+    print(task_names) # prints all task names
+
+    # no_dupes = [x for n, x in enumerate(a) if x not in a[:n]]
+    # print(no_dupes) # [[1], [2], [3], [5]]
+
+    # dupes = [x.text for n, x in enumerate(a) if x in a[:n]]
+    # print(dupes) # [[1], [3]]
+
+
+# def test_list_2():
+#     items = driver.find_elements_by_tag_name("li")
+#     count = 0
+
+#     for item in items:
+#         text = item.text
+#         count += 1
+
+#     print(text)
 
     # close browser
     driver.quit()
